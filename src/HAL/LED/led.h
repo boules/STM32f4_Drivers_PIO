0a83d5826e146bd3567f4a5a279a06a8d7f3d73c
boules-nova <boules170@gmail.com>
2024-04-02 19:03:05 +0200

    updating includes structure

--- src/HAL/LED/led.h
+++ src/HAL/LED/led.h
@@ -4,17 +4,11 @@
 #include "dio.h"
 #include "port.h"
 
-/**************Led Config*************/
-#define LED_MASK(PortNum, PinNum)		(PortNum<<4 | PinNum)
-
-/**************Led Config*************/
-#define		LED1		LED_MASK(PORTA, PIN4)
-#define		LED2		LED_MASK(PORTA, PIN5)
-#define		LED3		LED_MASK(PORTA, PIN6)
+/* Led ID Mask */
+#define LED_ID(PortNum, PinNum)		(PortNum<<4 | PinNum)
 
 
-
-
+/* Functions Declerations */
 void LED_on(uint8 ledId);
 void LED_off(uint8 ledId);
 void LED_toggle(uint8 ledId);
---
+++ src/HAL/LED/led_cfg.h
@@ -0,0 +1,16 @@
+#ifndef LED_CFG_H
+#define LED_CFG_H
+
+#include "led.h"
+
+/* User Defined LEDs */
+
+/**
+ * Enter your defined Leds here
+*/
+#define		GreenLED		LED_ID(PORTA, PIN4)
+#define		YellowLED		LED_ID(PORTA, PIN5)
+#define		RedLED			LED_ID(PORTA, PIN6)
+
+
+#endif
